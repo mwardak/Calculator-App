a4754d26afc08c98cd6ccadd18752e22119580e1
Maher Wardak <maher.wardak@gmail.com>
2019-12-09 15:58:14 -0500

    added queryselectors

---
+++ script.js
@@ -0,0 +1,27 @@
+
+
+
+// queryselector to get numbers,operators, equals, decimal, clear
+ numbers = document.querySelectorAll("numbers");
+ console.log(numbers);
+ operators = document.querySelectorAll("operators");
+ equals = document.querySelector("equals");
+ decimal = document.querySelector("decimal");
+ clear = document.querySelector("clear");
+ result = document.querySelector("result")
+
+
+// // create eventlistner for buttons
+document.addEventListener("click", function(e){
+   
+    
+
+    
+    
+
+
+
+});
+
+
+
