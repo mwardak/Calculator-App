dab525931cac874c95e77fbe0edb75004ebe9d49
Maher Wardak <maher.wardak@gmail.com>
2019-12-09 21:02:20 -0500

    trying to display number when clicked

--- script.js
+++ script.js
@@ -1,20 +1,33 @@
-
+// create variables for numbers,operators, equals, decimal, clear
+// let numbers = [0,1,2,3,4,5,6,7,8,9,];
+// let operators = ["x", "/", "+", "-"];
+// let equals = "=";
+// let decimal = ".";
+// let clear = "clear";
+// let result = "";
 
 
 // queryselector to get numbers,operators, equals, decimal, clear
- numbers = document.querySelectorAll("numbers");
- console.log(numbers);
- operators = document.querySelectorAll("operators");
- equals = document.querySelector("equals");
- decimal = document.querySelector("decimal");
- clear = document.querySelector("clear");
- result = document.querySelector("result")
+ let numbers = document.querySelectorAll("numbers").values;
+ const operators = document.querySelectorAll("operators");
+ const equals = document.querySelector("equals");
+ const decimal = document.querySelector("decimal");
+ const clear = document.querySelector("clear");
+ let result = document.querySelector("result")
 
 
 // // create eventlistner for buttons
 document.addEventListener("click", function(e){
+    for (i = 0; i < numbers .valu; i++) {
+        result = numbers[i];
+        console.log(result);
+    // if (numbers){
+       
+    //    console.log(result);
    
     
+}
+
 
     
     
