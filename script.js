17bc127475916d6f22a3c36f5fbcc94d9c4ab342
Maher Wardak <maher.wardak@gmail.com>
2019-12-14 08:27:30 -0500

    numbers and operators displaying

--- script.js
+++ script.js
@@ -6,33 +6,32 @@
 // let clear = "clear";
 // let result = "";
 
+// queryselector to get numbers, operators, equals, decimal, clear
+//  let btn_9 = document.getElementById("btn_9");
+//  let equals = document.querySelector(".equals");
+//  let decimal = document.querySelector(".decimal");
+//  let clear = document.querySelector(".clear");
 
-// queryselector to get numbers,operators, equals, decimal, clear
- let numbers = document.querySelectorAll(".numbers");
- let operators = document.querySelectorAll(".operators");
- let equals = document.querySelector(".equals");
- let decimal = document.querySelector(".decimal");
- let clear = document.querySelector(".clear");
- let result = document.querySelector(".result")
+let wrap = document.getElementById("wrap");
+let numbers = document.querySelectorAll(".numbers");
+let operators = document.querySelectorAll(".operators");
+let result = document.getElementById("result");
 
-// result = numbers
- 
-// // create eventlistner for buttons
-numbers.addEventListener("click", function(e){
-     
-    if (e.target.nodeName === "BUTTON"){
-       e.target.numbers = value;
+for (let i = 0; i < operators.length; i++) {
+  const compute = operators[i];
 
-     }
-       
-        
-    // }
-   
-    
-// }
+  compute.addEventListener("click", function(e) {
+    result.value += e.target.value;
+  });
+}
 
+for (let i = 0; i < numbers.length; i++) {
+  const btns = numbers[i];
 
-});
+  btns.addEventListener("click", function(e) {
+    // console.log(result.value);
 
-
+    result.value += e.target.value;
+  });
+}
 
