bad5c328b3864e39b4d3c0e29de2742d39b257fb
Maher Wardak <maher.wardak@gmail.com>
2019-12-14 20:50:52 -0500

    switch statement

--- script.js
+++ script.js
@@ -1,27 +1,47 @@
-// create variables for numbers,operators, equals, decimal, clear
-// let numbers = [0,1,2,3,4,5,6,7,8,9,];
-// let operators = ["x", "/", "+", "-"];
-// let equals = "=";
-// let decimal = ".";
-// let clear = "clear";
-// let result = "";
 
-// queryselector to get numbers, operators, equals, decimal, clear
+
+
 //  let btn_9 = document.getElementById("btn_9");
 //  let equals = document.querySelector(".equals");
 //  let decimal = document.querySelector(".decimal");
-//  let clear = document.querySelector(".clear");
+
+
+// queryselectors and getelementbyID
 
 let wrap = document.getElementById("wrap");
 let numbers = document.querySelectorAll(".numbers");
 let operators = document.querySelectorAll(".operators");
 let result = document.getElementById("result");
+let clear = document.getElementById("clear");
+
+
+
+// loop through operators and numbers and save it in a const
 
 for (let i = 0; i < operators.length; i++) {
   const compute = operators[i];
 
   compute.addEventListener("click", function(e) {
     result.value += e.target.value;
+
+    switch(operators){
+        case '/':
+      result.value = numbers / numbers;
+          console.log(result.value);
+          break;
+        case '*':
+         result.value = numbers * numbers;
+        case '+': 
+        result.value = numbers + numbers;
+          break;
+        case '-':
+        result.value = numbers - numbers;
+          break;
+        case '=':
+        result.value = "";
+          break;
+        }
+    
   });
 }
 
@@ -29,9 +49,18 @@ for (let i = 0; i < numbers.length; i++) {
   const btns = numbers[i];
 
   btns.addEventListener("click", function(e) {
-    // console.log(result.value);
+  
 
     result.value += e.target.value;
   });
 }
 
+// clear button 
+
+clear.addEventListener("click", function(e) {
+
+    if (clear) {
+        result.value = null;
+    }
+   
+  });
