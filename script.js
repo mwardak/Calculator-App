217b52fdae1c7c339bb13ba438ba2a7a3ad2ed16
Maher Wardak <maher.wardak@gmail.com>
2019-12-18 21:52:31 -0500

    operator should not display when clicked

--- script.js
+++ script.js
@@ -1,10 +1,6 @@
-
-
-
 //  let btn_9 = document.getElementById("btn_9");
-//  let equals = document.querySelector(".equals");
-//  let decimal = document.querySelector(".decimal");
 
+//  let decimal = document.querySelector(".decimal");
 
 // queryselectors and getelementbyID
 
@@ -13,34 +9,20 @@ let numbers = document.querySelectorAll(".numbers");
 let operators = document.querySelectorAll(".operators");
 let result = document.getElementById("result");
 let clear = document.getElementById("clear");
-
-
+let equals = document.getElementById("equals");
 
 // loop through operators and numbers and save it in a const
 
 for (let i = 0; i < operators.length; i++) {
   const compute = operators[i];
+  console.log(compute);
 
   compute.addEventListener("click", function(e) {
-    result.value += e.target.value;
-
-    switch(operators){
-        case '/':
-      result.value = numbers / numbers;
-          console.log(result.value);
-          break;
-        case '*':
-         result.value = numbers * numbers;
-        case '+': 
-        result.value = numbers + numbers;
-          break;
-        case '-':
-        result.value = numbers - numbers;
-          break;
-        case '=':
-        result.value = "";
-          break;
-        }
+    if (result.value = compute) {
+        result.value = null;
+    } else {
+      
+    }
     
   });
 }
@@ -49,18 +31,29 @@ for (let i = 0; i < numbers.length; i++) {
   const btns = numbers[i];
 
   btns.addEventListener("click", function(e) {
-  
+    // check if display is equal to zero
 
-    result.value += e.target.value;
+    if (result.value === "0") {
+      // if it is, then set the display equal to the button's value
+      result.value = e.target.value;
+      // else display more than double digits +
+    } else { 
+      result.value += e.target.value;
+    }
+  
+    // if (result.value) 
+    //     equals === e.target.value;
+    // } else {
+      
+    // }
+
   });
 }
 
-// clear button 
+// clear button
 
 clear.addEventListener("click", function(e) {
-
-    if (clear) {
-        result.value = null;
-    }
-   
-  });
+  if (clear) {
+    result.value = "0";
+  }
+});
