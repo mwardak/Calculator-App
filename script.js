859b1f9fc8851b6e7097e522e93fac6bfc57d93a
Maher Wardak <maher.wardak@gmail.com>
2019-12-29 22:38:40 -0500

    adding two numbers

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-//  let btn_9 = document.getElementById("btn_9");
-
-//  let decimal = document.querySelector(".decimal");
+// global variables
 let firstOperand = null;
 let secondOperand = null;
 let operation = null;
@@ -14,56 +12,53 @@ let result = document.getElementById("result");
 let clear = document.getElementById("clear");
 let equals = document.getElementById("equals");
 
+equals.addEventListener("click", function(e) {
+  // do a math operation using first and second operand
+  // display the result
+
+  if (operation !== null) {
+    equals = parseFloat(firstOperand) + parseFloat(secondOperand);
+    result.value = equals;
+  }
+});
 // loop through operators and numbers and save it in a const
-
 for (let i = 0; i < operators.length; i++) {
-  let compute = operators[i];
-
-  compute.addEventListener("click", function(e) {
-    if (compute) {
+  let operationButton = operators[i];
+  operationButton.addEventListener("click", function(e) {
+    if (operationButton) {
       firstOperand = result.value;
-      operation = firstOperand + compute;
-      
-      // store the screen value as the first operand
-      // RULE 2: Whatever number is on screen will be saved as either
-      // a.) first operator
-      // b.) second operator
-      // PSEUDO CODE:
-      // if there is currently a current operation
-      // then store the current display in the second operand variable
-      // else
-      // store the current display in the first operand variable
+      operation = firstOperand + secondOperand;
     }
   });
 }
-
 for (let i = 0; i < numbers.length; i++) {
   let numberButton = numbers[i];
-
   numberButton.addEventListener("click", function(e) {
-    // check if display is equal to zero
-
+    
     if (operation !== null) {
-      if (secondOperand === null) {
-        secondOperand = e.target.value;
-      } else {
-        secondOperand += e.target.value;
-      }
+      setSecondOperand(e.target.value);
       result.value = secondOperand;
     } else {
-      if (firstOperand === null) {
-        firstOperand = e.target.value;
-      } else {
-        firstOperand += e.target.value;
-      }
-
+      setFirstOperand(e.target.value);
       result.value = firstOperand;
     }
   });
 }
-
+function setFirstOperand(number) {
+  if (firstOperand === null) {
+    firstOperand = number;
+  } else {
+    firstOperand += number;
+  }
+}
+function setSecondOperand(number) {
+  if (secondOperand === null) {
+    secondOperand = number;
+  } else {
+    secondOperand += number;
+  }
+}
 // clear button
-
 clear.addEventListener("click", function(e) {
   if (clear) {
     result.value = "0";
